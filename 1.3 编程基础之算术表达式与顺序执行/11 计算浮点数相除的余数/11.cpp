0a08c1312aa2f09cd61fcd4f8288599493934585
z-Endeavor <zkczdd@163.com>
2019-08-12 23:30:01 +0800

    Update

---
+++ 1.3 编程基础之算术表达式与顺序执行/11 计算浮点数相除的余数/11.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	double a, b, r;
+	int k;
+	cin>>a>>b;
+	k = a / b;
+	r = a - k*b;
+	cout<<r;
+	return 0;
+}
---
+++ 1.3 编程基础之算术表达式与顺序执行/12 计算球的体积/12.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	double r, v, PI = 3.14;
+	cin>>r;
+	v = 4.0/3*PI*r*r*r;
+	printf("%.2lf", v);
+	return 0;
+}
